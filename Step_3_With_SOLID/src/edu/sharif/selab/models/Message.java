67136919cc8c7c92fb2917e2a9bc650b16ad9d28
fareedfotuhi <farfotfir@gmail.com>
2024-04-14 18:03:51 +0330

    change models

--- Step_3_With_SOLID/src/edu/sharif/selab/models/Message.java
+++ Step_3_With_SOLID/src/edu/sharif/selab/models/Message.java
@@ -1,8 +1,26 @@
 package edu.sharif.selab.models;
 
 public class Message {
-    String content;
 
+    String content;
+    private String source;
+    private String target;
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
     public String getContent() {
         return content;
     }
